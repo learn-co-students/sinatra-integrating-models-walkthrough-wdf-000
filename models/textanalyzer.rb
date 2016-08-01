557c55f745daef7e8bba28dca90f18b15e01a8f7
Raycent <github email address>
2016-08-01 13:23:34 -0400

    Done.

--- models/textanalyzer.rb
+++ models/textanalyzer.rb
@@ -1,1 +1,23 @@
-#your text analyzer model code will go here.
+class TextAnalyzer
+  attr_accessor :text
+
+  def initialize(text)
+    @text = text
+  end
+
+  def count_of_words
+    @text.split(" ").count
+  end
+
+  def count_of_vowels
+    @text.downcase.scan(/[aeoui]/).count
+  end
+
+  def count_of_consonants
+    @text.downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+  end
+
+  def most_used_letter
+    @text.downcase.gsub(/[^a-z]/, '').each_char.with_object(Hash.new) { |c, h| h[c] += 1 }.max_by(&:last)
+  end
+end
