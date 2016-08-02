3f6cd7c7e2e12279dc800e2a8b25641b1586b90c
aehmt <calisahmet@gmail.com>
2016-08-02 19:25:53 -0400

    Done.

--- models/textanalyzer.rb
+++ models/textanalyzer.rb
@@ -1,1 +1,27 @@
-#your text analyzer model code will go here.
+class TextAnalyzer 
+  attr_accessor :text 
+
+  def initialize(text)
+    @text = text
+  end 
+
+  def count_of_words
+    text.split(" ").size
+  end
+  
+  def count_of_vowels
+    text.scan(/[aAeEiIuUoO]/).size
+  end
+
+  def count_of_consonants
+    text.gsub!(/[aAeEiIuUoO ]/, "").length 
+  end
+
+  def most_used_letter
+    @arr = text.gsub(/[ ]/, "").split("").sort
+    arr.max_by { |i| arr.count(i) }
+  end
+  def occurence
+    @arr.count(@most_common_letter)
+  end
+end
