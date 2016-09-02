8bbf1e073607461b75c3950d2cc773db0ce579b2
Kelene Chung <simoy93@gmail.com>
2016-09-02 14:28:54 -0400

    Sinatra Integrating Models Walkthrough DONE

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require_relative 'config/environment'
 require_relative 'models/textanalyzer.rb'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -8,9 +9,8 @@ get '/' do
 end
 
 post '/' do
-  text_from_user = params[:user_text]
+  @analyzed_text = TextAnalyzer.new(params[:user_text])
   erb :results
 end
-
 
 end
