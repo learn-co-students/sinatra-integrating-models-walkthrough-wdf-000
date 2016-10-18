58535a36caa3809481d47362ff3b7145904e3173
Ekaterina Petushova <epetushova@gmail.com>
2016-10-18 12:43:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,14 +3,18 @@ require_relative 'models/textanalyzer.rb'
 
 class App < Sinatra::Base
 
-get '/' do
-  erb :index
-end
+	get '/' do
+  		erb :index
+	end
 
-post '/' do
-  text_from_user = params[:user_text]
-  erb :results
-end
-
-
+# post '/' do
+#   	text_from_user = params[:user_text]
+# 	@analyzed_text = TextAnalyzer.new(text_from_user)
+#   	erb :results
+# end
+# shorten version of sinatra method post:
+	post '/' do
+  		@analyzed_text = TextAnalyzer.new(params[:user_text])
+  		erb :results
+	end
 end
